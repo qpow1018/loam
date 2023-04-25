e0c8fecd091de08121add24d9bc98aedc2dd4002
qpow1018 <qpow1018@gamil.com>
2023-04-25 19:13:59 +0900

    remove webpack

--- src/index.js
+++
@@ -1,7 +0,0 @@
-// import { add, hello } from './util';
-// import './style.css';
-
-// const text = hello('원진gagasg');
-// const num = add(1, 2);
-
-// document.getElementById('root').innerHTML = text + num;
