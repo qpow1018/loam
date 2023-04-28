f72382276f384d10e1b0370af4ff49ee9f32041f
qpow1018 <qpow1018@gamil.com>
2023-04-28 17:40:38 +0900

    add snackbar lib

--- src/lib/snackbar.js
+++ src/lib/snackbar.js
@@ -1,22 +1,80 @@
 class Snackbar {
+  #hideSeconds = 2500;
   #displaySnackbars = [];
-  
-  
+
   success(message) {
-    document.body.insertAdjacentHTML(
-      'beforeend',
-      `
-        <div class="test-snackbar">
-          ${message}
-        </div>
-      `
-    );
+    const $elm = this.#makeElement('success', message);
+    this.#appendElement($elm);
+    this.#setupAutoRemoveFunc($elm);
   }
 
   error(message) {
-    
+    const $elm = this.#makeElement('error', message);
+    this.#appendElement($elm);
+    this.#setupAutoRemoveFunc($elm);
   }
 
+  #makeElement(type, message) {
+    const $elm = document.createElement('div');
+    $elm.classList.add('loam-snackbar');
+
+    let colorClassName = 'loam-snackbar-error';
+    let iconClassName = 'fa-solid fa-circle-exclamation';
+
+    if (type === 'success') {
+      colorClassName = 'loam-snackbar-success';
+      iconClassName = 'fa-solid fa-circle-check';
+    }
+
+    $elm.insertAdjacentHTML(
+      'afterbegin',
+      `
+        <div class="loam-snackbar-container ${colorClassName}">
+          <div class="loam-snackbar-icon">
+            <i class="${iconClassName}"></i>
+          </div>
+          <div class="loam-snackbar-message">
+            ${message}
+          </div>
+        </div>
+      `
+    );
+    return $elm;
+  }
+
+  #appendElement($elm) {
+    document.body.append($elm);
+    this.#displaySnackbars.unshift($elm);
+
+    console.log($elm.offsetHeight);
+    $elm.style.bottom = `-${$elm.offsetHeight}px`;
+    $elm.style.visibility = 'visible';
+
+    setTimeout(() => {
+      this.#updatePositions();
+    }, 10);
+  }
+
+  #updatePositions() {
+    let prevBottomPosition = 0;
+    this.#displaySnackbars.forEach($elm => {
+      $elm.style.bottom = `${prevBottomPosition}px`;
+      prevBottomPosition += $elm.offsetHeight;
+    })
+  }
+
+  #setupAutoRemoveFunc($elm) {
+    setTimeout(() => {
+      $elm.classList.add('loam-snackbar-hide');
+
+      // hide 애니메이션 실행 후 실제 요소 없애기
+      setTimeout(() => {
+        this.#displaySnackbars.pop();
+        $elm.remove();
+      }, 250);
+
+    }, this.#hideSeconds);
+  }
 }
 
 const _inst = new Snackbar()
