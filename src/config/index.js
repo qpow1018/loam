b0f71350c194da0fa18187d78c95085aaa3d8947
qpow1018 <56070196+qpow1018@users.noreply.github.com>
2023-05-23 15:28:56 +0900

    update config data

--- src/config/index.js
+++ src/config/index.js
@@ -1,93 +1,115 @@
 const config = {
   storageKey: {
     myCharacters: 'LOAM-MY-CHARACTERS',
-    mainLoado: 'MAIN-LOADO'
+    mainLoado: 'MAIN-LOADO',
+    mainCharacterLoado: 'MAIN-CHARACTER-LOADO'
   },
   data: {
     memo: {
       keyName: 'memo',
-      iconPath: '',
-      title: '메모'
+      resetType: 'none',
+      iconFileName: '',
+      title: '메모',
+      defaultCellType: 'text',
+      defaultValue: ''
     },
     dailyEpona: {
       keyName: 'dailyEpona',
       resetType: 'daily',
-      iconPath: '',
+      iconFileName: 'icon_dailyEpona.png',
       title: '에포나',
-      defaultValueType: 'checkbox'
+      defaultCellType: 'checkbox',
+      defaultValue: false
     },
     bloodstone: {
       keyName: 'bloodstone',
       resetType: 'daily',
       iconPath: '',
+      iconFileName: 'icon_bloodstone.png',
       title: '혈석',
-      defaultValueType: 'checkbox'
+      defaultCellType: 'checkbox',
+      defaultValue: false
     },
     affinity: {
       keyName: 'affinity',
       resetType: 'daily',
-      iconPath: '',
+      iconFileName: 'icon_affinity.png',
       title: '호감도',
-      defaultValueType: 'checkbox'
+      defaultCellType: 'checkbox',
+      defaultValue: false
     },
     chaosDungeon: {
       keyName: 'chaosDungeon',
       resetType: 'daily',
-      iconPath: '',
+      iconFileName: 'icon_chaosDungeon.png',
       title: '카오스 던전',
-      defaultValueType: 'relaxationBonus'
+      defaultCellType: 'relaxationBonus',
+      defaultValue: {
+        threshold: 0,
+        count: 0,
+        relaxationBonus: 0
+      }
     },
     guardianRaid: {
       keyName: 'guardianRaid',
       resetType: 'daily',
-      iconPath: '',
+      iconFileName: 'icon_guardianRaid.png',
       title: '가디언 토벌',
-      defaultValueType: 'relaxationBonus'
+      defaultCellType: 'relaxationBonus',
+      defaultValue: {
+        threshold: 0,
+        count: 0,
+        relaxationBonus: 0
+      }
     },
     weeklyEpona: {
       keyName: 'weeklyEpona',
       resetType: 'weekly',
-      iconPath: '',
+      iconFileName: 'icon_weeklyEpona.png',
       title: '주간 에포나',
-      defaultValueType: 'checkbox'
+      defaultCellType: 'checkbox',
+      defaultValue: false
     },
     bloodstoneExchange: {
       keyName: 'bloodstoneExchange',
       resetType: 'weekly',
-      iconPath: '',
+      iconFileName: 'icon_bloodstone.png',
       title: '혈석 교환',
-      defaultValueType: 'checkbox'
+      defaultCellType: 'checkbox',
+      defaultValue: false
     },
     cube: {
       keyName: 'cube',
       resetType: 'weekly',
-      iconPath: '',
+      iconFileName: 'icon_cube.png',
       title: '큐브',
-      defaultValueType: 'checkbox'
+      defaultCellType: 'checkbox',
+      defaultValue: false
     },
     endContents1: {
       keyName: 'endContents1',
       resetType: 'weekly',
-      iconPath: '',
+      iconFileName: 'icon_endContents.png',
       title: '엔드컨텐츠1',
-      defaultValueType: 'checkbox'
+      defaultCellType: 'checkbox',
+      defaultValue: false
     },
     endContents2: {
       keyName: 'endContents2',
       resetType: 'weekly',
-      iconPath: '',
+      iconFileName: 'icon_endContents.png',
       title: '엔드컨텐츠2',
-      defaultValueType: 'checkbox'
+      defaultCellType: 'checkbox',
+      defaultValue: false
     },
     endContents3: {
       keyName: 'endContents3',
       resetType: 'weekly',
-      iconPath: '',
+      iconFileName: 'icon_endContents.png',
       title: '엔드컨텐츠3',
-      defaultValueType: 'checkbox'
-    },
-
-
+      defaultCellType: 'checkbox',
+      defaultValue: false
+    }
   }
 }
 
